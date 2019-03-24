f12b08448120501644b8efaf006b739cd2bb9914
RandelSouza <randelsouza88@gmail.com>
2019-03-24 12:36:30 -0300

    "update menu.py"

--- menu.py
+++ menu.py
@@ -34,3 +34,4 @@ def changeOption( self, index, selected):
 if index < len(self.options) and  index >= 0:
 print self.options[index]["font"], self.options[index]["selected"]
 self.options[index]["selected"] = selected
+print self.options[index]["font"], self.options[index]["selected"]
