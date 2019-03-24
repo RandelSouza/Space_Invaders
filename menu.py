1e2bab66e12e98a751d622e3853bebbee9885a6f
RandelSouza <randelsouza88@gmail.com>
2019-03-24 12:33:59 -0300

    "update menu.py"

--- menu.py
+++ menu.py
@@ -7,3 +7,4 @@ def __init__( self ):
 self.init = pygame.init()
 self.font =  pygame.font.Font(None, 80)
 self.screen = pygame.display.set_mode( [ LARGURA, ALTURA ] )
+self.background =  pygame.image.load( "image/menu1.png" ).convert()
