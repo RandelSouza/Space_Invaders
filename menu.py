5a3dbbb68d4a38978d4a864d6f201e6bc7203cd2
RandelSouza <randelsouza88@gmail.com>
2019-03-24 12:37:17 -0300

    "update menu.py"

--- menu.py
+++ menu.py
@@ -43,3 +43,4 @@ self.screen.blit( font[ "font" ], font[ "pos" ] )
 def drawAndUpdateMenu( self ):
 self.screen.blit( self.background, ( 0, 0 ) )
 self.updateScreenOptionsFonts()
+pygame.display.update()
