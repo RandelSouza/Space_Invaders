609a5903752109f23c24f209867ee45ff88f330a
RandelSouza <randelsouza88@gmail.com>
2019-03-25 00:01:16 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -12,3 +12,4 @@ self.width = width
 self.height = height
 self.speed = speed
 self.image =  pygame.transform.scale(pygame.image.load("image/aliensprite1.png").convert_alpha(), (40,40))
+self.image2 = pygame.transform.scale(pygame.image.load("image/ufo2.png").convert_alpha(), (50,50))
