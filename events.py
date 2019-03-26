a6d184cdd64fb747afec07848540f4dfd0ea45e3
RandelSouza <randelsouza88@gmail.com>
2019-03-26 00:05:10 -0300

    "update events.py"

--- events.py
+++ events.py
@@ -58,3 +58,4 @@ return self.eventButtonMouseRight(event, main_game)
 def eventHandler(self, X, Y, shoot, max, main_game):
 for event in pygame.event.get():
 main_game = self.eventQuit( event )
+self.eventKeyDown( event, shoot, X, Y, max)
