00b788af7a073e2e7339293e0104944b1e213688
RandelSouza <randelsouza88@gmail.com>
2019-03-26 00:00:52 -0300

    "update events.py"

--- events.py
+++ events.py
@@ -7,3 +7,4 @@ def __init__(self):
 self.font =  pygame.font.Font('./score_board/scoreboard.ttf', 30)
 self.count = 0
 self.score = self.font.render("score: " + str(self.count), True, (0, 255, 0))
+
