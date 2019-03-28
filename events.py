072863dc894d2319626a3ae975576364016a0ca2
RandelSouza <randelsouza88@gmail.com>
2019-03-28 10:10:51 -0300

    "update events.py"

--- events.py
+++ events.py
@@ -15,7 +15,7 @@ class Events(object):
     def removeBulletShip(self, shoot, X, Y, max):
         if len(BULLETS) < max:
             BULLETS.append(pygame.Rect(X+36, Y, 25, 50))
-self.shootSound(shoot)
+            self.shootSound(shoot)
 
 def eventKeyPressK_Space(self, event, shoot, X, Y, max):
 if event.key == pygame.K_SPACE :
