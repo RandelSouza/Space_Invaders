c583c8523128a88cbc661d1984c4491ae49a60ea
RandelSouza <randelsouza88@gmail.com>
2019-03-27 22:01:16 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -44,7 +44,7 @@ class Enemy(object):
         collide = ship.get_rect().collidelist(BULLETS_ENEMY)
 
         if collide != -1:
-BULLETS_ENEMY.remove(BULLETS_ENEMY[collide])
+            BULLETS_ENEMY.remove(BULLETS_ENEMY[collide])
 if ship.heart_count >= 1:
 #ship.sound2.play()
 ship.heart_count -= 1
