4d202cbec9d3d6dbe98733994c36c665eb5cc92c
RandelSouza <randelsouza88@gmail.com>
2019-03-27 21:58:51 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -30,7 +30,7 @@ class Enemy(object):
                 count = 0
                 y2 += 35
 
-enemyObject = Enemy(LARGURA/2 - 250 + (count * 50), y2, 50, 50, 0.8)
+                enemyObject = Enemy(LARGURA/2 - 250 + (count * 50), y2, 50, 50, 0.8)
 enemies.append(enemyObject)
 count += 1
 
