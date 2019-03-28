bc8ee5545a83940767a3fd42f02eaf69f69c852b
RandelSouza <randelsouza88@gmail.com>
2019-03-27 21:55:51 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -5,7 +5,7 @@ from threading import Thread
 import time
 
 class Enemy(object):
-def __init__(self, x , y, width, height, speed):
+    def __init__(self, x , y, width, height, speed):
 self.x = x
 self.y = y
 self.width = width
