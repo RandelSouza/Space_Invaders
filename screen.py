dbe8d833a2178ca64b8c54aa5a206465bc682d0f
RandelSouza <randelsouza88@gmail.com>
2019-03-27 22:12:17 -0300

    "update screen.py"

--- screen.py
+++ screen.py
@@ -15,6 +15,6 @@ class Screen(object):
     def limites_tela(self, X, Y):
         if X <= -10: X = 702
         if X >= 704.99: X = 0
-if Y <= 422: Y = 422
+        if Y <= 422: Y = 422
 if Y > 499:   Y = 499
 return X, Y
