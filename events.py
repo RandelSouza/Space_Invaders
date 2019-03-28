c39cee3ce25612f18825fa01290d50a3292876c4
RandelSouza <randelsouza88@gmail.com>
2019-03-28 10:09:55 -0300

    "update events.py"

--- events.py
+++ events.py
@@ -6,7 +6,7 @@ class Events(object):
     def __init__(self):
         self.font =  pygame.font.Font('./score_board/scoreboard.ttf', 30)
         self.count = 0
-self.score = self.font.render("score: " + str(self.count), True, (0, 255, 0))
+        self.score = self.font.render("score: " + str(self.count), True, (0, 255, 0))
 
 def shootSound(self, shoot):
 shoot.sound.play()
