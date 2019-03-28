ce56ea66b8d8a084c7703f1f33e71c25a7079494
RandelSouza <randelsouza88@gmail.com>
2019-03-27 22:14:57 -0300

    "update menu.py"

--- menu.py
+++ menu.py
@@ -21,7 +21,7 @@ class Menu( object ):
         fonts = ["Play", "Settings", "Credits", "Exit"]
         posX = 320
         posY = 150
-selected = 0
+        selected = 0
 collor = "green"
 
 for nameFont in fonts:
