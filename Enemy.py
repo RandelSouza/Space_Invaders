1f7e542b1f4e31c408e87182b063b19c78446623
RandelSouza <randelsouza88@gmail.com>
2019-03-27 22:03:03 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -64,7 +64,7 @@ class Enemy(object):
         direcao = 1
 
         if len(enemies) != 0:
-for enemy in enemies:
+            for enemy in enemies:
 self.enemy_create_shot(shot_enemy, enemy.x, enemy.y)
 
 self.update_coordinate_enemy(enemy)
