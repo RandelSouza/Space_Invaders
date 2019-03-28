1a65be8ce82c473ba9372e2356bc94362d22a63a
RandelSouza <randelsouza88@gmail.com>
2019-03-28 10:09:11 -0300

    "update events.py"

--- events.py
+++ events.py
@@ -3,7 +3,7 @@ from setup import *
 from pygame.locals import *
 
 class Events(object):
-def __init__(self):
+    def __init__(self):
 self.font =  pygame.font.Font('./score_board/scoreboard.ttf', 30)
 self.count = 0
 self.score = self.font.render("score: " + str(self.count), True, (0, 255, 0))
