12184bde39a7220de9ee3e01eac79452304a49d5
RandelSouza <randelsouza88@gmail.com>
2019-03-27 22:00:33 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -37,7 +37,7 @@ class Enemy(object):
 
     def enemy_create_shot(self, shot_enemy, X, Y):
         if len(BULLETS_ENEMY) < random.randint(1, 2):
-BULLETS_ENEMY.append(pygame.Rect(X+16, Y-5, 25, 50))
+            BULLETS_ENEMY.append(pygame.Rect(X+16, Y-5, 25, 50))
 shot_enemy.sound.play()
 
 def enemy_collide(self, ship):
