252f02e18dbb0be78f67ec18c911ca39dfc7247b
RandelSouza <randelsouza88@gmail.com>
2019-03-27 21:57:10 -0300

    "update Enemy.py"

--- Enemy.py
+++ Enemy.py
@@ -14,7 +14,7 @@ class Enemy(object):
         self.image =  pygame.transform.scale(pygame.image.load("image/aliensprite1.png").convert_alpha(), (40,40))
         self.image2 = pygame.transform.scale(pygame.image.load("image/ufo2.png").convert_alpha(), (50,50))
         self.image3 = pygame.transform.scale(pygame.image.load("image/alien.png").convert_alpha(), (50,50))
-self.image4 = pygame.transform.scale(pygame.image.load("image/alien2.png").convert_alpha(), (50,50))
+        self.image4 = pygame.transform.scale(pygame.image.load("image/alien2.png").convert_alpha(), (50,50))
 self.rect = self.get_rect()
 
 def get_rect(self):
