af6653043c7d7e4068712d341c7078598faf014a
RandelSouza <randelsouza88@gmail.com>
2019-03-29 00:04:37 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -49,3 +49,4 @@ return pygame.Rect(self.x, self.y, self.width, self.height)
 def draw_centered(self, surface1, surface2, position):
 rect = surface1.get_rect()
 rect = rect.move( position[0], position[1] )
+surface2.blit( surface1, rect )
