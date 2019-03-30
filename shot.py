59e8cfef8b6471b634c158b73e2abe0fcc029077
RandelSouza <randelsouza88@gmail.com>
2019-03-30 00:00:54 -0300

    "update shot.py"

--- shot.py
+++ shot.py
@@ -8,3 +8,4 @@ def __init__(self):
 #self.image = pygame.transform.scale(pygame.image.load("image/enemy_shoot_purple.png").convert_alpha(), (25,25))
 self.image = []
 self.speed = 1
+self.sound = pygame.mixer.Sound("music/Shoot_00.wav")
