39370073ec582461f6b1b9a04bb748f4000b3266
RandelSouza <randelsouza88@gmail.com>
2019-03-31 00:00:43 -0300

    "update shot_enemy.py"

--- shot_enemy.py
+++ shot_enemy.py
@@ -6,3 +6,4 @@ class ShotEnemy(object):
 def __init__(self):
 self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25)), 180)
 self.image2 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("image/shot_yellow.png").convert_alpha(), (25,25)), 180)
+self.speed = 1.9
