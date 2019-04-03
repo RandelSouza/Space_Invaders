ad99cfbc97cc6f74a8d9ae83f6ec890b3a1c275a
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:57:27 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -81,7 +81,7 @@ class Ship(object):
         self.x  -= self.speed
 
     def setSpritNumber(self, sprit_number):
-self.sprit_number = sprit_number
+        self.sprit_number = sprit_number
 
 def setSoundMaxTime(self, maxTimeSound):
 self.sound.play(0, maxTimeSound)
