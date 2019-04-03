6a53b1194ed94744e3a088ba3de58bcd329d6516
RandelSouza <randelsouza88@gmail.com>
2019-04-03 11:55:43 -0300

    "update shot.py"

--- shot.py
+++ shot.py
@@ -7,7 +7,7 @@ class Shot(object):
         #self.image = pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25))
         #self.image = pygame.transform.scale(pygame.image.load("image/enemy_shoot_purple.png").convert_alpha(), (25,25))
         self.image = []
-self.speed = 1
+        self.speed = 1
 self.sound = pygame.mixer.Sound("music/Shoot_00.wav")
 self.sound2 = pygame.mixer.Sound("music/Explosion_01.wav")
 self.sound2.set_volume(0.1)
