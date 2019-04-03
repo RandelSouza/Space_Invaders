a68cc91be0a82e9aedffb5def948a5726ce7274a
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:56:45 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -75,9 +75,9 @@ class Ship(object):
             self.appendImageInArray( "image/ship%i.png" %( indexImage + 1) )
 
     def moveRight(self):
-self.x  += self.speed
+        self.x  += self.speed
 
-def moveLeft(self):
+    def moveLeft(self):
 self.x  -= self.speed
 
 def setSpritNumber(self, sprit_number):
