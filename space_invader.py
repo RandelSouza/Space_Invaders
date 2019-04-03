fb91c7251a2fdffc5cbe8158c3815c3f5d5e7040
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:27:13 -0300

    "update space_invader.py"

--- space_invader.py
+++ space_invader.py
@@ -34,7 +34,7 @@ while True:
             if main_game == 0:
                 break
 
-main_game = events.eventHandler(ship.x, ship.y, shot, max_shot, main_game)
+            main_game = events.eventHandler(ship.x, ship.y, shot, max_shot, main_game)
 
 tela.screen.blit(tela.background, (0,0))
 shot.update_bullets(tela.screen)
