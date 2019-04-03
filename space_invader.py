5cb5464018b8fab62bffadf11ceb6dab758ca4fa
RandelSouza <randelsouza88@gmail.com>
2019-04-03 12:30:01 -0300

    "update space_invader.py"

--- space_invader.py
+++ space_invader.py
@@ -42,14 +42,14 @@ while True:
 
             if enemies_quantity == 0:
                 enemy.create_enemies(number)
-            if spriteEnemy == 1:
-                spriteEnemy = 2
-            elif spriteEnemy == 2:
-                spriteEnemy = 3
-            elif spriteEnemy == 3:
-                spriteEnemy = 4
-            else:
-                spriteEnemy = 1
+                if spriteEnemy == 1:
+                    spriteEnemy = 2
+                elif spriteEnemy == 2:
+                    spriteEnemy = 3
+                elif spriteEnemy == 3:
+                    spriteEnemy = 4
+                else:
+                    spriteEnemy = 1
 
             enemies_quantity = enemy.update_enemies(tela.screen, spriteEnemy, shot, events, ship, shotEnemy)
 
