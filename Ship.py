6d90bef298bf32778e485958d60d87bd404d1ae7
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:44:04 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 class Ship(object):
     def __init__(self, x, y, width, height, speed, screen):
-self.screen = screen
+        self.screen = screen
 self.images = []
 self.speed = speed
 self.setWidthAndHeight( width, height )
