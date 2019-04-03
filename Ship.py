20ddf27d382e73eae5034be082321e488e1b9426
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:44:37 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -7,7 +7,7 @@ class Ship(object):
         self.images = []
         self.speed = speed
         self.setWidthAndHeight( width, height )
-self.setXandY(x, y)
+        self.setXandY(x, y)
 self.sprit_number = 1
 self.get_images_array()
 self.setSounds()
