0696a8e95e6a1535ad7392ab243cd7f55f0fd2b6
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:48:28 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -23,7 +23,7 @@ class Ship(object):
         self.sound = pygame.mixer.Sound("music/wind1.wav")
         self.sound2 = pygame.mixer.Sound("music/Explosion_01.wav")
 
-def setWidth(self, width):
+    def setWidth(self, width):
 self.width = width
 
 def setHeight(self, height):
