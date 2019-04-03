95523a7d0e3ae7c7610098d30b746bd03bbf047e
RandelSouza <randelsouza88@gmail.com>
2019-04-02 23:55:48 -0300

    "update Ship.py"

--- Ship.py
+++ Ship.py
@@ -69,7 +69,7 @@ class Ship(object):
 
     def get_images_array(self):
         for indexImage in range(1, 4):
-self.appendImageInArray( "image/redfighter000%i.png" %( indexImage + 3 ) )
+            self.appendImageInArray( "image/redfighter000%i.png" %( indexImage + 3 ) )
 
 for indexImage in range(1, 4):
 self.appendImageInArray( "image/ship%i.png" %( indexImage + 1) )
