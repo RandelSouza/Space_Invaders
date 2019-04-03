a0fb39eea2d295c73b8c216de496c06bc2dd374b
RandelSouza <randelsouza88@gmail.com>
2019-04-03 11:52:26 -0300

    "update shot_enemy.py"

--- shot_enemy.py
+++ shot_enemy.py
@@ -6,7 +6,7 @@ class ShotEnemy(object):
     def __init__(self):
         self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25)), 180)
         self.image2 = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("image/shot_yellow.png").convert_alpha(), (25,25)), 180)
-self.speed = 1.9
+        self.speed = 1.9
 self.sound = pygame.mixer.Sound("music/Shoot_00.wav")
 self.sound2 = pygame.mixer.Sound("music/Explosion_01.wav")
 self.sound2.set_volume(0.1)
