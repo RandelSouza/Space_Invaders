5068e965dc9bf798c6ca7b07dc66f51d90c2621d
RandelSouza <randelsouza88@gmail.com>
2019-04-03 11:55:22 -0300

    "update shot.py"

--- shot.py
+++ shot.py
@@ -4,7 +4,7 @@ from setup import *
 
 class Shot(object):
     def __init__(self):
-#self.image = pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25))
+        #self.image = pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25))
 #self.image = pygame.transform.scale(pygame.image.load("image/enemy_shoot_purple.png").convert_alpha(), (25,25))
 self.image = []
 self.speed = 1
