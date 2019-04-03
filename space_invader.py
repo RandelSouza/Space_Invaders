cf87506a6770fc4590e40fdd7e469890bf686b15
RandelSouza <randelsouza88@gmail.com>
2019-04-03 11:50:35 -0300

    "update space_invader.py"

--- space_invader.py
+++ space_invader.py
@@ -58,5 +58,5 @@ while True:
             ship.update_position()
             ship.update_heart()
 
-    tela.screen.blit(events.score,  (600, 0))
+            tela.screen.blit(events.score,  (600, 0))
     pygame.display.update()
