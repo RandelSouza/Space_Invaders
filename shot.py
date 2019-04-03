4dcc8a70fbf46bec8da36a77213a0f3eb77240a8
RandelSouza <randelsouza88@gmail.com>
2019-04-03 11:55:12 -0300

    "update shot.py"

--- shot.py
+++ shot.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 from setup import *
 
 class Shot(object):
-def __init__(self):
+    def __init__(self):
 #self.image = pygame.transform.scale(pygame.image.load("image/shot_red.png").convert_alpha(), (25,25))
 #self.image = pygame.transform.scale(pygame.image.load("image/enemy_shoot_purple.png").convert_alpha(), (25,25))
 self.image = []
@@ -33,4 +33,4 @@ if len(BULLETS) != 0:
 for bullet in BULLETS:
 self.moveBullet(bullet)
 self.removeBullet(bullet)
-self.drawBullet(self.image[4], bullet, screen)                            
+self.drawBullet(self.image[4], bullet, screen)
