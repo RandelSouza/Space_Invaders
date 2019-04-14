8875a90100db4dc0fdb5afaebabc90f64725b259
RandelSouza <randelsouza88@gmail.com>
2019-04-14 11:35:42 -0300

    "update"

--- menu.py
+++ menu.py
@@ -48,7 +48,7 @@ class Menu( object ):
                 self.screen.blit( self.renderFont( font[ "font" ][ 0 ], font[ "font" ] [ 1 ] ), font[ "pos" ] )
 
     def printOptions(self, index):
-        selfnameFonte = self.options[index]["font"][0]
+        nameFonte = self.options[index]["font"][0]
 
         if nameFont == "Exit":
             exit()
