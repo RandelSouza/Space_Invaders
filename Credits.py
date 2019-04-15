270fec64807236bcaa659e15010daf91458823b1
RandelSouza <randelsouza88@gmail.com>
2019-04-15 00:05:22 -0300

    "update"

--- Credits.py
+++ Credits.py
@@ -5,10 +5,10 @@ from pygame.locals import *
 class Credits( object ):
     def __init__( self ):
         self.init = pygame.init()
-        self.font =  pygame.font.Font( None, 50 )
+        self.font =  pygame.font.Font( None, 40 )
         self.screen = pygame.display.set_mode( [ LARGURA, ALTURA ] )
         self.text = []
-        self.collors = { "white": (255, 255, 255), "green": ( 0, 255, 0 ) }
+        self.collors = { "white" : (255, 255, 255), "green" : ( 0, 255, 0 ) }
         self.setText()
         pygame.font.init()
         self.limit = ALTURA
@@ -20,28 +20,36 @@ class Credits( object ):
         text = [
                  "Game Programming - Randel Souza ALmeida",
                  "Game Design - Randel Souza Almeida",
-                 "Picture Art - GameArt site (www.gameArt.com)",
-                 "                                    CREDITS" ]
+                 "Site (https://opengameart.org/)",
+                 "Picture Art - OpenGameArt: ",
+                 "                                   CREDITS" ]
 
         posX = 20
         posY = ALTURA + 300
 
         for nameText in text:
             self.text.append( { "text" : nameText, "pos" : ( posX, posY ), "collor" : self.collors[ "white" ] } )
-            posY -= 100
+            posY -= 50
 
     def updateScreenText( self ):
         for text in self.text:
             self.screen.blit( self.renderText( text[ "text" ], text[ "collor" ] ), (text[ "pos" ][ 0 ], text[ "pos" ][1]) )
-            text[ "pos" ] = ( text[ "pos" ][0], text[ "pos" ][1] - 0.1)
-            print text[ "pos" ]
+            text[ "pos" ] = ( text[ "pos" ][0], text[ "pos" ][1] - 0.3)
+            if text[ "pos" ][1] <= -50:
+                text[ "pos" ] = ( text[ "pos" ][0], ALTURA + 300)
+
+    def resetText( self ):
+        self.text = []
+        self.setText()
 
     def eventQuitCredits( self, event ):
         if event.type == QUIT:
+            self.resetText()
             return 3
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                self.resetText()
                 return 3
 
     def eventHandlerCredits( self ):
