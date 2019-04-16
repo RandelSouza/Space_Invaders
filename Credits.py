dd70db1637a3b5407231489fd510e9287b729afe
RandelSouza <randelsouza88@gmail.com>
2019-04-16 17:37:55 -0300

    "update"

--- Credits.py
+++ Credits.py
@@ -18,7 +18,7 @@ class Credits( object ):
 
     def setText( self ):
         text = [
-                 "Game Logic "
+                 "Game Logic -  "
                  "Game Programming - Randel Souza ALmeida",
                  "Game Design - Randel Souza Almeida",
                  "Site (https://opengameart.org/)",
