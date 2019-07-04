e7ebf024e8bb4e16ca975960bfeb97e5b8e9c3ee
RandelSouza <randelsouza88@gmail.com>
2019-07-04 20:54:40 -0300

    "update enemy_create_shot"

--- Enemy.py
+++ Enemy.py
@@ -38,7 +38,7 @@ class Enemy(object):
         print time.time() - start_t
 
     def enemy_create_shot(self, shot_enemy, X, Y):
-        if len(BULLETS_ENEMY) < random.randint(1, 10):
+        if len(BULLETS_ENEMY) < random.randint(1, 2):
             BULLETS_ENEMY.append(pygame.Rect(X+16, Y-5, 25, 50))
             shot_enemy.sound.play(0)
 
